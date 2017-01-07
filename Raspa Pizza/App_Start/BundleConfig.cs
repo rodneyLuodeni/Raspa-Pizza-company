72408471c900e337388f8a97a078f5a9ca2d99cb
rodney <rodney22@live.nl>
2017-01-08 00:44:19 +0100

    Created the header for the pages and imported google fonts

--- Raspa Pizza/App_Start/BundleConfig.cs
+++ Raspa Pizza/App_Start/BundleConfig.cs
@@ -5,9 +5,12 @@ namespace Raspa_Pizza
 {
     public class BundleConfig
     {
+
+        
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
